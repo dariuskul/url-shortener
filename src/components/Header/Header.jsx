76fa43a48f9767d593b65cdc6a6a3f8aa8ede568
Darius <dariuux@gmail.com>
2021-06-22 14:05:09 +0300

    inital commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+import './header.css'
+const Header = () => {
+    return(
+        <header>
+            <h1 className="header__title">Shortly</h1>
+            <div className="header__menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
