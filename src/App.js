72f579c0bef689caa018a418017d67ed957ea244
Darius <dariuux@gmail.com>
2021-06-27 19:15:00 +0300

    starter section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Features from "./components/Features/Features";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Input from "./components/Input/Input";
+import Starter from "./components/Starter/Starter";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero/>
       <Input/>
       <Features/>
+      <Starter/>
     </div>
   );
 }
---
+++ src/components/Starter/Starter.jsx
@@ -0,0 +1,13 @@
+import './starter.css'
+const Starter = () => {
+    return(
+        <section className="starter__section">
+            <div className="starter__wrapper">
+                <h3>Boost your links today</h3>
+                <button>Get started</button>
+            </div>
+        </section>
+    )
+}
+
+export default Starter;
