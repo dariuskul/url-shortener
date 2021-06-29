c41cc81144b736c14747b8e9faed7b774db260de
Darius <dariuux@gmail.com>
2021-06-29 22:50:11 +0300

    small fixes

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import './link.css'
 const Link = ({originalLink, shortenedLink}) => {
     const [clicked,setClicked] = useState(false);
